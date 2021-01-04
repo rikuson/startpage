0aa72242be8e8cc6dff448f4ec3af12eae7352c5
Riku Takeuchi <rikuson@users.noreply.github.com>
2021-01-04 23:11:43 +0900

    Add twitter search

--- src/CommandLine.js
+++ src/CommandLine.js
@@ -1,16 +1,50 @@
 import 'bootswatch/dist/slate/bootstrap.min.css';
+import 'jquery';
+import 'bootstrap';
+import { Component } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faGoogle, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-function CommandLine() {
+function DropdownItem(props) {
   return (
-    <form className="input-group" action="//www.google.com/search" method="GET">
-      <input name="q" className="form-control" style={{ margin: 1 }} type="search" placeholder="Search..." />
-      <div className="input-group-append">
-        <button className="btn btn-secondary" type="submit"><FontAwesomeIcon icon={faGoogle} /></button>
-      </div>
-    </form>
+    <li><a className="dropdown-item" href={`${props.url}?${props.param}=${props.value}`}><FontAwesomeIcon icon={props.icon} /> {props.name}</a></li>
   );
+}
+
+class CommandLine extends Component {
+  static get SEARCH_ENGINES() {
+    return [
+      { name: 'Google', icon: faGoogle, url: 'https://www.google.com/search', param: 'q' },
+      { name: 'Twitter', icon: faTwitter, url: 'https://twitter.com/search', param: 'q' },
+    ];
+  }
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+    };
+  }
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({ value });
+  }
+  render() {
+    const dft = CommandLine.SEARCH_ENGINES[0];
+    return (
+      <form className="input-group" action={dft.url} method="GET">
+        <input name={dft.param} className="form-control" style={{ margin: 1 }} type="search" placeholder="Search..." onChange={e => this.handleChange(e)} value={this.state.value} />
+        <div className="input-group-append">
+          <div className="btn-group" role="group">
+            <button className="btn btn-secondary" style={{ borderRadius: 0 }} type="submit"><FontAwesomeIcon icon={dft.icon} /></button>
+            <button id="searchEngineDropdown" type="button" className="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+            <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="searchEngineDropdown">
+              {CommandLine.SEARCH_ENGINES.map((props, i) => <DropdownItem key={i} value={this.state.value} {...props} />)}
+            </ul>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default CommandLine;
