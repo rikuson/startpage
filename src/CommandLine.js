fc3d4b3804e1895f426f5194f4741f408a14c2b4
Riku Takeuchi <rikuson@users.noreply.github.com>
2021-01-04 19:35:50 +0900

    Add icons

--- src/CommandLine.js
+++ src/CommandLine.js
@@ -1,11 +1,13 @@
 import 'bootswatch/dist/slate/bootstrap.min.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 function CommandLine() {
   return (
     <form className="input-group" action="//www.google.com/search" method="GET">
-      <input name="q" className="form-control" style={{ margin: 1 }} type="search" />
+      <input name="q" className="form-control" style={{ margin: 1 }} type="search" placeholder="Search..." />
       <div className="input-group-append">
-        <button className="btn btn-secondary" type="submit">Search</button>
+        <button className="btn btn-secondary" type="submit"><FontAwesomeIcon icon={faGoogle} /></button>
       </div>
     </form>
   );
