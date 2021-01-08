fb6bf8f7233b7eb8b4db31ff1002f94e1c905ebc
Riku Takeuchi <rikuson@users.noreply.github.com>
2021-01-08 13:43:33 +0900

    Rename project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CommandLine from './CommandLine';
 function App() {
   return (
     <div className={styles.container}>
-      <h1 className="display-3">Start Page</h1>
+      <h1 className="display-3">Browxin</h1>
       <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
       <CommandLine />
     </div>
