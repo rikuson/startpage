3da5ec21e6fd17b7061335687af63706d1bd6970
Riku Takeuchi <rikuson@users.noreply.github.com>
2021-01-09 18:11:15 +0900

    Update lead sentence

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import 'bootswatch/dist/slate/bootstrap.min.css';
 import styles from './App.module.css';
 import CommandLine from './CommandLine';
+import ContentEditable from 'react-contenteditable';
 
 function App() {
   return (
     <div className={styles.container}>
       <h1 className="display-3">Browxin</h1>
-      <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <p className="lead"><ContentEditable html="Browse in cross-platform!" /></p>
       <CommandLine />
     </div>
   );
