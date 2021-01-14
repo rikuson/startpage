80616a94f1c9a276f22a1a749cb2e228835030de
Riku Takeuchi <rikuson@users.noreply.github.com>
2021-01-14 16:23:27 +0900

    Uninstall content editable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import 'bootswatch/dist/slate/bootstrap.min.css';
 import styles from './App.module.css';
 import CommandLine from './CommandLine';
-import ContentEditable from 'react-contenteditable';
 
 function App() {
   return (
     <div className={styles.container}>
       <h1 className="display-3">Browxin</h1>
-      <p className="lead"><ContentEditable html="Browse in cross-platform!" /></p>
+      <p className="lead">Browse in cross-platform!</p>
       <CommandLine autoFocus={true} />
     </div>
   );
